b2d770cf2c2318c31f8498d541abc25334ba4c0f
Garv Jhajharia <garvjhajharia@gmail.com>
2024-12-28 19:58:13 +0530

    Update supabase.js

--- trip-planner-app/frontend/src/supabase.js
+++ trip-planner-app/frontend/src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
